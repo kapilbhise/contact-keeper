1dd3b0ff857e00795ef923a4a7876789bf6fe49d
Kapil Bhise <bhisekapil38@gmail.com>
2020-12-31 13:22:45 +0530

    prepared fro deployment

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,82 @@
+import React, { Fragment, useContext, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+import ContactContext from '../../context/contact/contactContext';
+
+const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+  const contactContext = useContext(ContactContext);
+
+  const { isAuthenticated, logout, user, loadUser } = authContext;
+  const { clearContacts } = contactContext;
+
+  useEffect(() => {
+    loadUser();
+    // eslint-disable-next-line
+  }, []);
+
+  const onLogout = () => {
+    logout();
+    clearContacts();
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li>
+        <strong>Hello {user && user.name}</strong>
+      </li>
+      <li>
+        <a onClick={onLogout} href='#!'>
+          <i className='fas fa-sign-out-alt' />{' '}
+          <span className='hide-sm'>
+            <strong>Logout</strong>
+          </span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to='/about'>
+          <strong>About</strong>
+        </Link>
+      </li>
+      <li>
+        <Link to='/register'>
+          <strong>Register</strong>
+        </Link>
+      </li>
+      <li>
+        <Link to='/login'>
+          <strong>Login</strong>
+        </Link>
+      </li>
+    </Fragment>
+  );
+
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <Link to='/'>
+          <i className={icon} /> {title}
+        </Link>
+      </h1>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: 'Contact Keeper',
+  icon: 'fas fa-id-card-alt',
+};
+
+export default Navbar;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>
+        Full Stack MERN App to store your contacts.User can login and can search his contacts.
+      </p>
+    </div>
+  );
+};
+
+export default About;
